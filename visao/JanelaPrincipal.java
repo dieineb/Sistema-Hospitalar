82d6fc4372c69d92dcfde915a5b89b33476d02e2
AndreiamFerreira <58631470+AndreiamFerreira@users.noreply.github.com>
2021-03-06 15:25:44 -0300

    Update JanelaPrincipal.java

--- visao/JanelaPrincipal.java
+++ visao/JanelaPrincipal.java
@@ -30,8 +30,48 @@ public class JanelaPrincipal extends JFrame {
 	 * Create the frame.
 	 */
 	public JanelaPrincipal() {
+		setTitle("Sistema de Atendimento Hospitalar");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu menuPaciente = new JMenu("Paciente");
+		menuBar.add(menuPaciente);
+		
+		JMenuItem itemGerenciarPaciente = new JMenuItem("Gerenciar paciente");
+		menuPaciente.add(itemGerenciarPaciente);
+		
+		JMenu menuAtendimentoEmergencia = new JMenu("Atendimento Emerg\u00EAncia");
+		menuBar.add(menuAtendimentoEmergencia);
+		
+		JMenuItem itemGerarAtendimento = new JMenuItem("Gerar atendimento");
+		menuAtendimentoEmergencia.add(itemGerarAtendimento);
+		
+		JMenu menuConsulta = new JMenu("Consulta");
+		menuBar.add(menuConsulta);
+		
+		JMenuItem itemGerarConsulta = new JMenuItem("Gerar consulta");
+		menuConsulta.add(itemGerarConsulta);
+		
+		JMenuItem itemEncaminhar = new JMenuItem("Encaminhar para interna\u00E7\u00E3o");
+		menuConsulta.add(itemEncaminhar);
+		
+		JMenuItem itemFinalizarAtendimento = new JMenuItem("Finalizar Atendimento");
+		menuConsulta.add(itemFinalizarAtendimento);
+		
+		JMenu menuRelatorios = new JMenu("Relat\u00F3rios");
+		menuBar.add(menuRelatorios);
+		
+		JMenuItem itemListarPacientesAguardando = new JMenuItem("Listar pacientes aguardando leito (por ala)");
+		menuRelatorios.add(itemListarPacientesAguardando);
+		
+		JMenuItem itemListarInternados = new JMenuItem("Listar pacientes internados (por ala)");
+		menuRelatorios.add(itemListarInternados);
+		
+		JMenuItem itemListarLeitosVagos = new JMenuItem("Listar n\u00FAmero de leitos vagos (por ala)");
+		menuRelatorios.add(itemListarLeitosVagos);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
