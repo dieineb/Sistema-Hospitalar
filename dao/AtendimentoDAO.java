a973668e8233ecd0baafb47344019de770369fa6
moyses-santos <moyzlevi@gmail.com>
2021-03-12 20:33:48 -0300

    Adicionado funcao AtendimentoDAO no txt

--- dao/AtendimentoDAO.java
+++ dao/AtendimentoDAO.java
@@ -1,20 +1,33 @@
 package dao;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import modelo.Atendimento;
+import modelo.Paciente;
 
 public class AtendimentoDAO {
-	private List<Atendimento> atendimentosNaEnfermaria;
+	//Quatros filas de espera para cada prioridade e uma enfermaria de emergencia
+	private static List<Atendimento> enfermariaDeEmergencia = new ArrayList();
+	private static List<Atendimento> filaDeEspera1 = new ArrayList();
+	private static List<Atendimento> filaDeEspera2 = new ArrayList();
+	private static List<Atendimento> filaDeEspera3 = new ArrayList();
+	private static List<Atendimento> filaDeEspera4 = new ArrayList();
 	
-	private List<Atendimento> filaDeEspera1 = new ArrayList();
-	private List<Atendimento> filaDeEspera2 = new ArrayList();
-	private List<Atendimento> filaDeEspera3 = new ArrayList();
-	private List<Atendimento> filaDeEspera4 = new ArrayList();
+	//Alas para o atendimento
+	private static List<Atendimento> cardiologia = new ArrayList();
+	private static List<Atendimento> padiatrica = new ArrayList();
+	private static List<Atendimento> pneumologia = new ArrayList();
+	private static List<Atendimento> neurologia = new ArrayList();
 	
+	//path para adicionar no txt
+	private final static String txtASerMudado = "atendimentos";
+	private final static String path = UtilDAO.pathParaDataMultiSO(txtASerMudado);
 	
-	public boolean gerarAtendimento(Atendimento atendime) {
+	public static boolean gerarAtendimento(Atendimento atendime) {
 		switch(atendime.getPrioridade()) {
 			case 1:
 				filaDeEspera1.add(atendime);
@@ -28,6 +41,49 @@ public class AtendimentoDAO {
 			case 4:
 				filaDeEspera4.add(atendime);
 				return true;
+			default:
+				return false;}
+			
+		}
+	
+	public static boolean gerarAtendimento(Atendimento atendime, String ala) throws Exception {
+			if(atendime.getPrioridade() == 5) {
+				if(ala.equals("cardiologia")) {
+					if(cardiologia.size()<=5) {
+						cardiologia.add(atendime);
+					}else {
+						if(enfermariaDeEmergencia.size()<=10) {
+							enfermariaDeEmergencia.add(atendime);
+						}else {
+							
+						}
+					}
+					
+				}					
+				return true;
+			}else {
+				throw new Exception("ERRO: O parâmetro ala não foi inserido corretamente");
+			}
+	}
+	
+	
+	
+	
+	public boolean adicionarAtendimentoNoTxtRemoverDoHospital(Atendimento atendime) {
+
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+			bw.write(String.format("%s,%s,%s,%d,%s,%s,%s", atendime.getPessoa().getNome(),
+					atendime.getPessoa().getCpf(), atendime.getData(), 
+					atendime.getPrioridade(), atendime.getQueixa(), 
+					atendime.getAla(),"paciente encaminhado para outro hospital por indisponibilidade de leitos"));
+			bw.newLine();
+			return true;
+
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
 		}
 	}
+	
+	
 }
