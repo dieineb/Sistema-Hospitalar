34a2c8b485d49a1d80fc166cbef654d218dc7cdf
moyses-santos <moyzlevi@gmail.com>
2021-03-12 17:12:04 -0300

    Adicionado encapsulamento para DAO, multi Sistema operacional para DAO e recuperação de dados do Txt para o programa

--- dao/PacienteDAO.java
+++ dao/PacienteDAO.java
@@ -1,51 +1,62 @@
 package dao;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import modelo.Paciente;
 
-public class PacienteDAO {		
+public class PacienteDAO {
+	private List<Paciente> pacientesNoHospital;
 
-	File arquivo = new File("pacientes.txt");
+	private final static String txtASerMudado = "pacientes";
+	private final static String path = UtilDAO.pathParaDataMultiSO(txtASerMudado);
 	
+	
+	//se quiser adicionar um paciente utilizar este método, ele tanto adiciona no txt quando na lista presente
+	// no programa o outro metodo é apenas para uma melhor organizacao do codigo
 	public boolean cadastraPaciente(Paciente p) {
-		try {
-			
-			arquivo = new File("pacientes.txt");
-			
-			FileWriter fw = new FileWriter(arquivo, true);
-			BufferedWriter bw = new BufferedWriter(fw);
-			
-			bw.write("CPF: " + p.getCpf());
+	
+	this.pacientesNoHospital.add(p);
+	
+	return this.adicionarPacienteNoTxt(p.getNome(), p.getCpf(), p.getDataNasc(), 
+			p.getEndereco(), p.getTipoSanguineo(), p.getNomePai(), p.getNomeMae());		
+	}
+	
+	
+	
+	public boolean adicionarPacienteNoTxt(String nmPaciente,String cpf ,String dtNascimento, String endereco, String tpSanguineo,
+			String nmPai, String nmMae) {
+
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+			bw.write(String.format("%s,%s,%d,%s,%s,%s,%s", nmPaciente,cpf, dtNascimento, endereco, tpSanguineo, nmPai, nmMae));
 			bw.newLine();
-			bw.write("Nome: " + p.getNome());
-			bw.newLine();
-			bw.write("Data de Nascimento: " + p.getDataNasc());
-			bw.newLine();
-			bw.write("Endereço: " + p.getEndereco());
-			bw.newLine();
-			bw.write("Nome do Pai: " + p.getNomePai());
-			bw.newLine();
-			bw.write("Nome da Mãe: " + p.getNomeMae());
-			bw.newLine();
-			bw.write("Tipo sanguíneo: " + p.getTipoSanguineo());
-			bw.newLine();
-			bw.write("--------------------------------------------------");
-			bw.newLine();
-			
-			bw.close();
-			fw.close();
-			
 			return true;
-			
-		} catch (Exception e) {
+
+		} catch (IOException e) {
 			e.printStackTrace();
-			
 			return false;
 		}
 	}
-	
-}
 
+	public void recuperarPacientesDoTxt() {
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			String line;
+			String[] values;
+			while ((line = br.readLine()) != null) {
+				values = line.split(",");
+		   		this.pacientesNoHospital.add(new Paciente(values[0],values[1],
+		   				values[2],values[3], values[4],values[5],values[6]));
+			}
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}		
+	}
+
+}
