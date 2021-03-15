b756d455ca7748fe6beafd17473215c00bc9a83a
AndreiamFerreira <58631470+AndreiamFerreira@users.noreply.github.com>
2021-03-15 15:53:42 -0300

    Update App.java

--- controle/App.java
+++ controle/App.java
@@ -7,12 +7,12 @@ import visao.JanelaPrincipal;
 public class App {
 public static void main(String[] args) {
 
-//    JanelaPrincipal janela = new JanelaPrincipal();
-//    janela.setVisible(true);
-//
-//    ControlePrincipal controlePrincipal = new ControlePrincipal(janela);
+   JanelaPrincipal janela = new JanelaPrincipal();
+	janela.setVisible(true);
+
+    ControlePrincipal controlePrincipal = new ControlePrincipal(janela);
 	PacienteDAO pDAO = new PacienteDAO();
-	pDAO.cadastraPaciente(new Paciente("teste","teste","teste","teste","teste","teste","teste"));
+	//pDAO.cadastraPaciente(new Paciente("teste","teste","teste","teste","teste","teste","teste"));
 	
 }
     
