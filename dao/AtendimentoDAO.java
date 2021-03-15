37fa5f23e5fb5aa8c6beac79e1e2fab397e5e72c
moyses-santos <moyzlevi@gmail.com>
2021-03-15 19:55:48 -0300

    Finalizado o DAO

--- dao/AtendimentoDAO.java
+++ dao/AtendimentoDAO.java
@@ -3,31 +3,31 @@ package dao;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.LinkedList;
 
 import modelo.Atendimento;
-import modelo.Paciente;
 
 public class AtendimentoDAO {
 	// Quatros filas de espera para cada prioridade e uma enfermaria de emergencia
-	private static List<Atendimento> enfermariaDeEmergencia = new ArrayList();
-	private static List<Atendimento> filaDeEspera1 = new ArrayList();
-	private static List<Atendimento> filaDeEspera2 = new ArrayList();
-	private static List<Atendimento> filaDeEspera3 = new ArrayList();
-	private static List<Atendimento> filaDeEspera4 = new ArrayList();
+	private static LinkedList<Atendimento> enfermariaDeEmergencia = new LinkedList<>();
+	private static LinkedList<Atendimento> filaDeEspera1 = new LinkedList<>();
+	private static LinkedList<Atendimento> filaDeEspera2 = new LinkedList<>();
+	private static LinkedList<Atendimento> filaDeEspera3 = new LinkedList<>();
+	private static LinkedList<Atendimento> filaDeEspera4 = new LinkedList<>();
 
 	// Alas para o atendimento
-	private static List<Atendimento> cardiologia = new ArrayList();
-	private static List<Atendimento> pediatrica = new ArrayList();
-	private static List<Atendimento> pneumologia = new ArrayList();
-	private static List<Atendimento> neurologia = new ArrayList();
+	private static LinkedList<Atendimento> cardiologia = new LinkedList<>();
+	private static LinkedList<Atendimento> pediatrica = new LinkedList<>();
+	private static LinkedList<Atendimento> pneumologia = new LinkedList<>();
+	private static LinkedList<Atendimento> neurologia = new LinkedList<>();
 
 	// Fila de espera por ala
-	private static List<Atendimento> FilaDeEsperaCardiologia = new ArrayList();
-	private static List<Atendimento> FilaDeEsperaPediatrica = new ArrayList();
-	private static List<Atendimento> FilaDeEsperaPneumologia = new ArrayList();
-	private static List<Atendimento> FilaDeEsperaNeurologia = new ArrayList();
+	private static LinkedList<Atendimento> filaDeEsperaCardiologia = new LinkedList<>();
+	private static LinkedList<Atendimento> filaDeEsperaPediatrica = new LinkedList<>();
+	private static LinkedList<Atendimento> filaDeEsperaPneumologia = new LinkedList<>();
+	private static LinkedList<Atendimento> filaDeEsperaNeurologia = new LinkedList<>();
 
 	// path para adicionar no txt
 	private final static String txtASerMudado = "atendimentos";
@@ -52,15 +52,54 @@ public class AtendimentoDAO {
 		}
 		return false;
 	}
-
 	public static boolean adicionaPacienteNaAla(Atendimento atendime) {
 
 		if (atendime.getAla().equals("cardiologia")) {
 			if (cardiologia.size() < 3) {
 				cardiologia.add(atendime);
 				return true;
+			} else {
+				filaDeEsperaCardiologia.add(atendime);
+				return true;
+			}
+		} else if (atendime.getAla().equals("pediatrica")) {
+			if (pediatrica.size() < 6) {
+				pediatrica.add(atendime);
+				return true;
+			} else {
+				filaDeEsperaPediatrica.add(atendime);
+				return true;
+			}
+
+		} else if (atendime.getAla().equals("pneumologia")) {
+			if (pneumologia.size() < 4) {
+				pneumologia.add(atendime);
+				return true;
+			} else {
+				filaDeEsperaPneumologia.add(atendime);
+				return true;
+			}
+		} else if (atendime.getAla().equals("neurologia")) {
+			if (neurologia.size() < 6) {
+				neurologia.add(atendime);
+				return true;
+			} else {
+				filaDeEsperaNeurologia.add(atendime);
+				return true;
+			}
+		}
+
+		return false;
+
+	}
+	public static boolean adicionaPacienteNaAlaPrioridade5(Atendimento atendime) {
+
+		if (atendime.getAla().equals("cardiologia")) {
+			if (cardiologia.size() < 3) {
+				cardiologia.add(atendime);
+				return true;
 			} else if (enfermariaDeEmergencia.size() < 10) {
-				FilaDeEsperaCardiologia.add(atendime);
+				filaDeEsperaCardiologia.add(atendime);
 				enfermariaDeEmergencia.add(atendime);
 			} else {
 				adicionarItemTxtRemoverDoHospital(atendime);
@@ -71,7 +110,7 @@ public class AtendimentoDAO {
 				pediatrica.add(atendime);
 				return true;
 			} else if (enfermariaDeEmergencia.size() < 10) {
-				FilaDeEsperaPediatrica.add(atendime);
+				filaDeEsperaPediatrica.add(atendime);
 				enfermariaDeEmergencia.add(atendime);
 			} else {
 				adicionarItemTxtRemoverDoHospital(atendime);
@@ -83,7 +122,7 @@ public class AtendimentoDAO {
 				pneumologia.add(atendime);
 				return true;
 			} else if (enfermariaDeEmergencia.size() < 10) {
-				FilaDeEsperaPneumologia.add(atendime);
+				filaDeEsperaPneumologia.add(atendime);
 				enfermariaDeEmergencia.add(atendime);
 			} else {
 				adicionarItemTxtRemoverDoHospital(atendime);
@@ -94,14 +133,14 @@ public class AtendimentoDAO {
 				neurologia.add(atendime);
 				return true;
 			} else if (enfermariaDeEmergencia.size() < 10) {
-				FilaDeEsperaNeurologia.add(atendime);
+				filaDeEsperaNeurologia.add(atendime);
 				enfermariaDeEmergencia.add(atendime);
 			} else {
 				adicionarItemTxtRemoverDoHospital(atendime);
 				return false;
 			}
 		}
-		
+
 		return false;
 
 	}
@@ -110,7 +149,7 @@ public class AtendimentoDAO {
 
 		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
 			bw.write(String.format("%s,%s,%s,%d,%s,%s,%s", atendime.getPessoa().getNome(),
-					atendime.getPessoa().getCpf(), atendime.getDhInternacao(), atendime.getPrioridade(),
+					atendime.getPessoa().getCpf(), atendime.getDhAtendimento(), atendime.getPrioridade(),
 					atendime.getQueixa(), atendime.getAla(),
 					"paciente encaminhado para outro hospital por indisponibilidade de leitos"));
 			bw.newLine();
@@ -120,6 +159,103 @@ public class AtendimentoDAO {
 			e.printStackTrace();
 			return false;
 		}
+	}
+	
+	public static boolean finalizarAtendimento(Atendimento atendime) {
+		
+		enfermariaDeEmergencia.remove(atendime);
+		if (atendime.getAla().equals("cardiologia")) {
+			filaDeEsperaCardiologia.remove(atendime);
+			
+		} else if (atendime.getAla().equals("pediatrica")) {
+			filaDeEsperaPediatrica.remove(atendime);
+
+		} else if (atendime.getAla().equals("pneumologia")) {
+			filaDeEsperaPneumologia.remove(atendime);
+			
+		} else if (atendime.getAla().equals("neurologia")) {
+			filaDeEsperaNeurologia.remove(atendime);
+		}
+		return adicionarConsultaNoTxt(atendime);
+	}
+	
+	public static boolean adicionarConsultaNoTxt(Atendimento atendime) {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+		LocalDateTime now = LocalDateTime.now();  
+		atendime.setDhTerminoAtendimento(now.toString());
+		
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+			bw.write(String.format("%s,%s,%s,%d,%s,%s,%s,%s,%s,%s", 
+					atendime.getPessoa().getNome(),
+					atendime.getPessoa().getCpf(), 
+					atendime.getDhAtendimento(), 
+					atendime.getPrioridade(),
+					atendime.getQueixa(), 
+					atendime.getAla(), 
+					atendime.getAvaliacaoMedica(),
+					atendime.getMedicacaoPrescrita(),
+					atendime.getObs(),
+					atendime.getDhTerminoAtendimento()));
+			bw.newLine();
+			return true;
+
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public static boolean encaminharParaInternacao(Atendimento atendime, String ala) {
+		int prioridade = atendime.getPrioridade();
+		switch(prioridade) {
+		case 1:
+			filaDeEspera1.remove(atendime);
+			break;
+		case 2:
+			filaDeEspera2.remove(atendime);
+			break;
+		case 3:
+			filaDeEspera3.remove(atendime);
+			break;
+		case 4: 
+			filaDeEspera4.remove(atendime);
+			break;
+		case 5:
+			enfermariaDeEmergencia.remove(atendime);
+			if (atendime.getAla().equals("cardiologia")) {
+				filaDeEsperaCardiologia.remove(atendime);
+				
+			} else if (atendime.getAla().equals("pediatrica")) {
+				filaDeEsperaPediatrica.remove(atendime);
+
+			} else if (atendime.getAla().equals("pneumologia")) {
+				filaDeEsperaPneumologia.remove(atendime);
+				
+			} else if (atendime.getAla().equals("neurologia")) {
+				filaDeEsperaNeurologia.remove(atendime);
+			}
+			break;
+		}
+		
+		atendime.setAla(ala);
+		return adicionaPacienteNaAla(atendime);
+	}
+
+	public static Atendimento recuperarAtendimentoPorPrioridade() {
+		if (!filaDeEspera4.isEmpty()) {
+			return filaDeEspera4.getFirst();
+		
+		} else if (!filaDeEspera3.isEmpty()) {
+			return filaDeEspera3.getFirst();
+		
+		} else if (!filaDeEspera3.isEmpty()) {
+			return filaDeEspera3.getFirst();
+		
+		}else if (!filaDeEspera1.isEmpty()) {
+			return filaDeEspera1.getFirst();
+		
+		}
+		return null;
 	}
 
 }
