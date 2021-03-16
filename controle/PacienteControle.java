0fc652be06e0132140fad8468a82c6dad716a39a
Andréia Ferreira <58631470+AndreiamFerreira@users.noreply.github.com>
2021-03-16 15:40:23 -0300

    Update PacienteControle.java
    Fiz um encadeamento pra verificar se os campos estavam em branco, e se sim, qual
    Comentei o método limpar, já que na TelaGerenciar já havia outro método

--- controle/PacienteControle.java
+++ controle/PacienteControle.java
@@ -3,14 +3,16 @@ package controle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import dao.AtendimentoDAO;
 import dao.PacienteDAO;
 import modelo.Atendimento;
 import modelo.Paciente;
 import visao.JanelaPrincipal;
+import visao.TelaAtendimento;
 import visao.TelaGerenciarPaciente;
 
 public class PacienteControle implements ActionListener {
-
+	
 	TelaGerenciarPaciente telaPaciente;
 
 	public PacienteControle(TelaGerenciarPaciente telaPaciente) {
@@ -19,46 +21,69 @@ public class PacienteControle implements ActionListener {
 
 		
 		this.telaPaciente.getButtonCadastrar().addActionListener(this);
-		this.telaPaciente.getButtonLimpar().addActionListener(this);
+	//	this.telaPaciente.getButtonLimpar().addActionListener(this);
+	
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		Paciente paciente = obterCamposPaciente();
+	
 		// TODO Auto-generated method stub
-
-		if (e.getActionCommand().equals("cadastrar-paciente")) {
-			System.out.println("CADASTRO");
-			Paciente paciente = obterCamposPaciente();
-
-			if (paciente == null) {
-				System.out.println("Preencha todos os campos!");
-			} else {
+		
+		
+		if(e.getActionCommand().equals("cadastrar-paciente")) {
+			
+			if(telaPaciente.getFieldCPF().getText().equals("")) {
+				System.out.println("CPF não inserido");
+			}
+			else if(telaPaciente.getFieldNome().getText().equals("")) {
+				System.out.println("Nome não inserido");
+			}
+			else if(telaPaciente.getFieldNascimento().getText().equals("")) {
+				System.out.println("Data de nascimento não inserida");
+			}
+		
+			else if(telaPaciente.getFieldEndereco().getText().equals("")) {
+				System.out.println("Endereco não inserido");
+			}
+			else if(telaPaciente.getFieldPai().getText().equals("")) {
+				System.out.println("Nome do pai não inserido");
+			}
+			else if(telaPaciente.getFieldMae().getText().equals("")) {
+				System.out.println("Nome da mae nao inserido");
+			}
+			
+			else if(telaPaciente.getComboBoxTipoSanguineo().getSelectedItem().equals("")) {
+				System.out.println("Tipo sanguíneo não selecionado");
+			}
+			else{
+				obterCamposPaciente();
 				boolean retorno = PacienteDAO.cadastraPaciente(paciente); 
 
 				if (retorno) {
 					System.out.println("Paciente cadastrado com sucesso!");
 				} else {
 					System.out.println("Não foi possível cadastrar!");
-				}
-
-				limparCamposPaciente();
-			}
-
+					
+			};
+			
 		}
-
-		if (e.getActionCommand().equals("limpar-tela")) {
-
-			limparCamposPaciente();
-
 		}
+		//if(e.getActionCommand().equals("Limpar")) {
+			//limparCamposPaciente();
 
+	
+		//}
+		
 	}
+
+
 
 	public Paciente obterCamposPaciente() {
 
 		Paciente paciente = null;
 
-		if (verificarCamposPaciente()) {
 
 			String cpf = telaPaciente.getFieldCPF().getText();
 			String nome = telaPaciente.getFieldNome().getText();
@@ -69,12 +94,12 @@ public class PacienteControle implements ActionListener {
 			String tipoSanguineo = (String) telaPaciente.getComboBoxTipoSanguineo().getSelectedItem();
 		
 			paciente = new Paciente(cpf, nome, dataNasc, endereco, nomePai, nomeMae, tipoSanguineo);
-		}
+		
 
 		return paciente;
 	}
 
-	public boolean verificarCamposPaciente() {
+	/*public boolean verificarCamposPaciente() {
 
 		if (telaPaciente.getFieldCPF().getText() == "" | telaPaciente.getFieldNome().getText() == ""
 				| telaPaciente.getFieldNascimento().getText() == "" | telaPaciente.getFieldEndereco().getText() == ""
@@ -83,9 +108,9 @@ public class PacienteControle implements ActionListener {
 		}
 
 		return true;
-	}
+	}*/
 
-	public void limparCamposPaciente() {
+	/*public void limparCamposPaciente() {
 			
 			telaPaciente.getFieldCPF().setText("");
 			telaPaciente.getFieldNome().setText("");
@@ -93,11 +118,12 @@ public class PacienteControle implements ActionListener {
 			telaPaciente.getFieldEndereco().setText("");
 			telaPaciente.getFieldPai().setText("");
 			telaPaciente.getFieldMae().setText("");
-			telaPaciente.getComboBoxTipoSanguineo().setSelectedItem("A+");
+			telaPaciente.getComboBoxTipoSanguineo().setSelectedItem("");
 			
-		}
+		}*/
 
 	}
+
 
 
 
