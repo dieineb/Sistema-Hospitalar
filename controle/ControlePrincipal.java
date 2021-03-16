57ace9ee5c63581045a90dd9b3d2760b129a26e3
AndreiamFerreira <58631470+AndreiamFerreira@users.noreply.github.com>
2021-03-15 23:03:21 -0300

    Update ControlePrincipal.java

--- controle/ControlePrincipal.java
+++ controle/ControlePrincipal.java
@@ -19,32 +19,40 @@ public class ControlePrincipal implements ActionListener {
 	TelaGerenciarPaciente telaPaciente = new TelaGerenciarPaciente();
 	
 	//instanciei as outras telas
+	ListaPaciente listaPacientes = new ListaPaciente();
 	TelaAtendimento telaAtendimento = new TelaAtendimento();
 	TelaConsulta telaConsulta = new TelaConsulta();
 	TelaEncaminharInternacao telaEncaminhar = new TelaEncaminharInternacao();
+	TelaFinalizarAtendimento telaFinalizar = new TelaFinalizarAtendimento();
 	TelaAguardando telaAguardar = new TelaAguardando();
 	TelaPacientesInternados telaInternados = new TelaPacientesInternados();
 	TelaLeitosVagos telaLeitos = new TelaLeitosVagos();
-
-	PacienteControle pacienteControle;
+	
 
 	PacienteDAO pacienteDAO = new PacienteDAO();
 
 	public ControlePrincipal(JanelaPrincipal janela) {
 		this.janela = janela;
-		PacienteDAO.recuperarPacientesDoTxt();
-		this.janela.getContentPane().add(telaPaciente, "painel2"); 
 		
 		//"puxei" os outros paineis aqui ao inves da janelaPrincipal
-		this.janela.getContentPane().add(telaAtendimento, "painel3");
-		this.janela.getContentPane().add(telaConsulta, "painel4");
-		this.janela.getContentPane().add(telaEncaminhar, "painel5");
+		this.janela.getContentPane().add(telaPaciente, "painel1"); 
+		this.janela.getContentPane().add(telaAtendimento, "painel2");
+		this.janela.getContentPane().add(telaConsulta, "painel3");
+		this.janela.getContentPane().add(telaEncaminhar, "painel4");
+		this.janela.getContentPane().add(telaFinalizar, "painel5");
 		this.janela.getContentPane().add(telaAguardar, "painel6");			
 		this.janela.getContentPane().add(telaInternados, "painel7");
 		this.janela.getContentPane().add(telaLeitos, "painel8");
-		//this.janela.getItemGerenciarPaciente().addActionListener(this)
-
+		
 		this.janela.getItemGerenciarPaciente().addActionListener(this);
+		this.janela.getItemGerarAtendimento().addActionListener(this);
+		this.janela.getItemGerarConsulta().addActionListener(this);
+		this.janela.getItemEncaminhar().addActionListener(this);
+		this.janela.getItemListarPacientesAguardando().addActionListener(this);
+		this.janela.getItemListarInternados().addActionListener(this);
+		this.janela.getItemListarLeitosVagos().addActionListener(this);
+		
+		
 	}
 
 	@Override
